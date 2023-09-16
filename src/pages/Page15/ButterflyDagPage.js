1af9401f507562bd08d27ce6dfbc774c387f83c4
zhangsy <zhangsy@think-land.com>
2023-09-16 17:22:46 +0800

    feat: code update

--- src/pages/Page15/ButterflyDagPage.js
+++ src/pages/Page15/ButterflyDagPage.js
@@ -3,8 +3,11 @@ import styled from "styled-components";
 import { Tabs } from 'antd'
 import BFDagConstant from "./BFDagConstant";
 import { DEFAULT_TAB_HEIGHT, findPageContentHeight } from "../utils/utils";
-import Demo1 from './component/Demo1/index'
-import Demo2 from './component/Demo2/index'
+import {
+  Demo1, Demo2, Demo3, Demo4, Demo5, Demo6, Demo7, Demo8, Demo9, Demo10,
+  Demo11, Demo12, Demo13, Demo14, Demo15, Demo16, Demo17, Demo18, Demo19, Demo20,
+  Demo21, Demo22, Demo23, Demo24, Demo25, Demo26, Demo27, Demo28
+} from './Page15Index'
 
 const ButterflyDagPage = () => {
   const [ activeKey, setActiveKey ] = useState(BFDagConstant.TABS_MAP.TAB2.key)
@@ -47,6 +50,240 @@ const ButterflyDagPage = () => {
             children: (
               <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
                 <Demo2 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB3.label,
+            key: BFDagConstant.TABS_MAP.TAB3.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo3 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB4.label,
+            key: BFDagConstant.TABS_MAP.TAB4.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo4 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB5.label,
+            key: BFDagConstant.TABS_MAP.TAB5.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo5 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB6.label,
+            key: BFDagConstant.TABS_MAP.TAB6.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo6 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB7.label,
+            key: BFDagConstant.TABS_MAP.TAB7.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo7 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB8.label,
+            key: BFDagConstant.TABS_MAP.TAB8.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo8 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB9.label,
+            key: BFDagConstant.TABS_MAP.TAB9.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo9 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB10.label,
+            key: BFDagConstant.TABS_MAP.TAB10.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo10 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB11.label,
+            key: BFDagConstant.TABS_MAP.TAB11.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo11 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB12.label,
+            key: BFDagConstant.TABS_MAP.TAB12.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo12 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB13.label,
+            key: BFDagConstant.TABS_MAP.TAB13.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo13 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB14.label,
+            key: BFDagConstant.TABS_MAP.TAB14.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo14 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB15.label,
+            key: BFDagConstant.TABS_MAP.TAB15.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo15 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB16.label,
+            key: BFDagConstant.TABS_MAP.TAB16.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo16 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB17.label,
+            key: BFDagConstant.TABS_MAP.TAB17.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo17 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB18.label,
+            key: BFDagConstant.TABS_MAP.TAB18.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo18 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB19.label,
+            key: BFDagConstant.TABS_MAP.TAB19.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo19 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB20.label,
+            key: BFDagConstant.TABS_MAP.TAB20.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo20 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB21.label,
+            key: BFDagConstant.TABS_MAP.TAB21.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo21 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB22.label,
+            key: BFDagConstant.TABS_MAP.TAB22.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo22 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB23.label,
+            key: BFDagConstant.TABS_MAP.TAB23.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo23 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB24.label,
+            key: BFDagConstant.TABS_MAP.TAB24.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo24 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB25.label,
+            key: BFDagConstant.TABS_MAP.TAB25.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo25 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB26.label,
+            key: BFDagConstant.TABS_MAP.TAB26.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo26 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB27.label,
+            key: BFDagConstant.TABS_MAP.TAB27.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo27 />
+              </div>
+            )
+          },
+          {
+            label: BFDagConstant.TABS_MAP.TAB28.label,
+            key: BFDagConstant.TABS_MAP.TAB28.key,
+            children: (
+              <div className={'tab-child-content'} style={{height: `${tabChildHeight}px`, overflowY: 'scroll'}}>
+                <Demo28 />
               </div>
             )
           },
---
+++ src/pages/Page15/Page15Index.js
@@ -0,0 +1,35 @@
+import Demo1 from './component/Demo1/index'
+import Demo2 from './component/Demo2/index'
+import Demo3 from './component/Demo3/index'
+import Demo4 from './component/Demo4/index'
+import Demo5 from './component/Demo5/index'
+import Demo6 from './component/Demo6/index'
+import Demo7 from './component/Demo7/index'
+import Demo8 from './component/Demo8/index'
+import Demo9 from './component/Demo9/index'
+import Demo10 from './component/Demo10/index'
+import Demo11 from './component/Demo11/index'
+import Demo12 from './component/Demo12/index'
+import Demo13 from './component/Demo13/index'
+import Demo14 from './component/Demo14/index'
+import Demo15 from './component/Demo15/index'
+import Demo16 from './component/Demo16/index'
+
+import Demo17 from './component2/Demo17/index'
+import Demo18 from './component2/Demo18/index'
+import Demo19 from './component2/Demo19/index'
+import Demo20 from './component2/Demo20/index'
+import Demo21 from './component2/Demo21/index'
+import Demo22 from './component2/Demo22/index'
+import Demo23 from './component2/Demo23/index'
+import Demo24 from './component2/Demo24/index'
+import Demo25 from './component2/Demo25/index'
+import Demo26 from './component2/Demo26/index'
+import Demo27 from './component2/Demo27/index'
+import Demo28 from './component2/Demo28/index'
+
+export {
+  Demo1, Demo2, Demo3, Demo4, Demo5, Demo6, Demo7, Demo8, Demo9, Demo10,
+  Demo11, Demo12, Demo13, Demo14, Demo15, Demo16, Demo17, Demo18, Demo19, Demo20,
+  Demo21, Demo22, Demo23, Demo24, Demo25, Demo26, Demo27, Demo28,
+}
