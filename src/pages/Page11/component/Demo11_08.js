eee69b6e94d0e60123a76374d9e626b45bbfdd6f
SUYANG\Admin <vambknight@gmail.com>
2023-09-26 00:07:20 +0800

    feat: code commit

--- src/pages/Page11/component/Demo11_08.js
+++ src/pages/Page11/component/Demo11_08.js
@@ -20,21 +20,76 @@ import { generateRandomArr } from '../../utils/utils'
  */
 const Demo11_08 = () => {
   const [ randomArr, setRandomArr ] = useState([])
+  const [ actionArr, setActionArr ] = useState([])
 
   const initRandomArr = () => {
     const randomArr = generateRandomArr(10, 100)
     setRandomArr(randomArr)
+    const actionArr = JSON.parse(JSON.stringify(randomArr))
+    heapSort(actionArr)
+    setActionArr(actionArr)
   }
 
   useEffect(()=>{
     initRandomArr()
   },[])
 
+  /**
+   * 一次移动（从上往下移动）
+   * @param arr 完整的数组
+   * @param low 移动的起始坐标（初始是顶层）
+   * @param high 移动的结束坐标（初始是最后一个叶子节点）
+   */
+  const sift = (arr, low, high) => {
+    // if(high === 1) {
+    //   debugger
+    // }
+    let i = low // 最开始指向的根节点
+    let j = 2 * i + 1 // 左子节点
+    const temp = arr[low] // 要移动的数据
+    while (j <= high && low !== high) {
+      if(j + 1 <= high && arr[j+1] > arr[j]) { //如果右子节点没有越界，且右子节点比左子节点大
+        j = j + 1 // 确定要比较的是右子节点
+      }
+      if(arr[j] > temp) { // 如果更大的子节点比较大
+        arr[i] = arr[j] // 这个更大的子节点往上移动
+        i = j
+        j = 2 * i + 1
+      }else { // 要比较的数据比两个子节点都大，那么这个数据就放在当前位置，循环结束
+        arr[i] = temp
+        break
+      }
+    }
+    arr[i] = temp
+  }
+
+
+  const heapSort = (arr) => {
+    const arrLength = arr.length
+    // 获取最后一个父节点的坐标，得到后所有需要调整的坐标就是 0 开始，到 lastSiftNodeIdx 结束
+    const lastSiftNodeIdx = (arrLength -1) % 2 === 1? (arrLength /2 -1): (arrLength - 3) /2
+
+    for(let i=lastSiftNodeIdx;i >= 0; i--) {
+      sift(arr, i, arrLength-1) // 可以简单的用左右一个叶子节点作为最后一个节点，这么做不用算，不会错
+    }
+    //================= 到这里，构造堆的任务完成 =======================
+
+    for(let i = arrLength -1; i >= 0; i--) {
+      // if(i === 1) {
+      //   debugger
+      // }
+      const temp = arr[i]
+      arr[i] = arr[0]
+      arr[0] = temp
+      sift(arr, 0, i-1 >0? i-1: 1) // 因为最后一位已经被原列表的第一个赋值了，且不需要在排序了，顾而是 i-1
+    }
+  }
+
   return (
     <UnitContent title={'11_08 堆排序'} disBtn={false}>
       <Wrapper>
         <div>{`Random Array: ${randomArr}`}</div>
-        {/*<div>{`Random Array after: ${insertSort(randomArr)}`}</div>*/}
+        <div>{`Random Array after: ${actionArr}`}</div>
       </Wrapper>
     </UnitContent>
   )
