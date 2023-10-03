84121120dbd20b5bd8d5f510d7b192b9d7ef1386
SUYANG\Admin <vambknight@gmail.com>
2023-10-03 22:05:54 +0800

    feat: code commit

--- src/pages/Page11/component/Demo11_09_2.js
+++ src/pages/Page11/component/Demo11_09_2.js
@@ -9,7 +9,17 @@ const Demo11_09_2 = () => {
   const divideSort = function(nums, target) {
     let left = 0, right = nums.length - 1;
     while(left <= right) {
-      let mid = (left + right) >>> 1;
+      /**
+       * 假定
+       * left = 200;
+       * right = 250;
+       * 则left+right =450 > 255，此时已经溢出了
+       * 0001 1100 0010 因为只能存储8位，实际1100 0010=194
+       * mid = (left+right)/2;  //此时实际mid=194/2
+       * mid = left+(right-left)/2; //200+(250-200)/2 = 225
+       * 此方法绝对不会溢出，最好写成这样
+       */
+      let mid = left + ((right - left) >> 1)
       if(nums[mid] < target) {
         left = mid + 1;
       }else if(nums[mid] > target) {
