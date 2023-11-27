4dcd84039585d20bb8553662cc0d37619ae2e333
zhangsy <zhangsy@think-land.com>
2023-11-27 15:51:39 +0800

    feat: code update

--- src/pages/Page8/componentV3/Demo34_0.js
+++ src/pages/Page8/componentV3/Demo34_0.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from "styled-components";
 import UnitContent from "../../components/UnitContent";
 import { Button } from "antd";
@@ -10,7 +10,7 @@ import pngFile from './react_file/2023_roadMap.png'
 import pdfFile from './react_file/ppp2.pdf'
 import jpgFile from './react_file/ToledoIldefonso_ZH-CN4507206651_1920x1080.jpg'
 import docxFile from './react_file/docx.docx'
-import docx2File from './react_file/docx2.doc'
+import docx2File from './react_file/docx2.docx'
 import txtFile from './react_file/txt.txt'
 import pdf2 from './react_file/pdf2.pdf'
 import docFile from './react_file/doc.doc'
@@ -19,11 +19,12 @@ const Demo34_0 = () => {
   const [ fileType, setFileType ] = useState(null)
   const [ isPdf1, setIsPdf1 ] = useState(true)
   const [ isDocx1, setIsDocx1 ] = useState(true)
+  const [ randomKey, setRandomKey ] = useState(Math.random())
   console.log('isDocx1', isDocx1)
 
-  const modalClose = () => {
-    setFileType(false)
-  }
+  useEffect(()=>{
+    setRandomKey(Math.random())
+  },[isDocx1])
 
   return (
     <>
@@ -32,7 +33,7 @@ const Demo34_0 = () => {
           <Button type={'primary'} onClick={()=>setIsPdf1(!isPdf1)}>change pdf</Button>
           <iframe src={isPdf1? pdfFile: pdf2} width={800} height={800} />
           <Button type={'primary'} onClick={()=>setIsDocx1(!isDocx1)}>change docx</Button>
-          <div style={{width: 800, height: 800, overflowY: "auto", backgroundColor: 'lightyellow'}}>
+          <div style={{width: 800, height: 800, overflowY: "auto", backgroundColor: 'lightyellow'}} key={randomKey}>
             <FileViewer
               styled={{width: 800, height: 800}}
               fileType='docx'//文件类型
