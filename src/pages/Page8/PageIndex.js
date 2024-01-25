1943550363a59c04cfd91ee82f1467cbf4e27c72
zhangsy <zhangsy@think-land.com>
2024-01-25 18:00:52 +0800

    feat: code commit

--- src/pages/Page8/PageIndex.js
+++ src/pages/Page8/PageIndex.js
@@ -74,6 +74,7 @@ import Demo29_3 from "./componentV3/Demo29_3";
 import Demo32_0 from "./componentV3/Demo32_0/Demo32_0";
 import Demo32_1 from "./componentV3/Demo32_0/Demo32_1";
 import Demo32_2 from "./componentV3/Demo32_0/Demo32_2";
+import Demo32_3 from "./componentV3/Demo32_0/Demo32_3";
 import Demo33_0 from "./Demo33_0/Demo33_0";
 import Demo34_0 from "./componentV3/Demo34_0";
 
@@ -87,5 +88,5 @@ export {
   Demo28_2, Demo28_3, Demo28_4, Demo28_5, Demo28_6, Demo28_7, Demo28_8, Demo28_9,
   Demo28_10, Demo29_0, Demo29_1, Demo29_2, Demo30_0, Demo30_1, Demo31_0, Demo31_1, Demo31_2, Demo31_3,
   Demo30_2, Demo28_11, Demo28_12, Demo28_13, Demo28_14, Demo28_15, Demo28_16, Demo28_17,
-  Demo29_3, Demo32_0, Demo32_1, Demo32_2, Demo33_0, Demo34_0
+  Demo29_3, Demo32_0, Demo32_1, Demo32_2, Demo32_3, Demo33_0, Demo34_0
 }
