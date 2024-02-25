f5adb43e597cca46c42223430783aeab542e740d
zhangsy <zhangsy@think-land.com>
2024-02-25 10:54:59 +0800

    feat: code update

--- src/pages/SinglePage/IntersectionPage.js
+++ src/pages/SinglePage/IntersectionPage.js
@@ -6,9 +6,14 @@ const IntersectionPage = () => {
   useEffect(()=>{
     let observer = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
+        // if (entry.isIntersecting) {
+        //   entry.target.classList.add('animate')
+        //   observer.unobserve(entry.target)
+        // }
         if (entry.isIntersecting) {
           entry.target.classList.add('animate')
-          observer.unobserve(entry.target)
+        }else {
+          entry.target.classList.remove('animate')
         }
       })
     })
