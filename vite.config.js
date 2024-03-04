a642fba9cce65d80287deec61b47886498b896bb
zhangsy <zhangsy@think-land.com>
2024-03-04 10:43:29 +0800

    feat: code update

--- vite.config.js
+++ vite.config.js
@@ -6,112 +6,114 @@ import { resolve } from 'path'
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  assetsInclude: [
-    '**/*.awebp', '**/*.webp', '**/*.jpg', '**/*.jpeg',
-    '**/*.png', '**/*.docx', '**/*.doc', '**/*.pdf', '**/*.xlsx', '**/*.txt',
-    '**/*.pptx',
-  ],
-  resolve: {
-    alias: {
-      'src': resolve(__dirname,'src'),
-    }
-  },
-  esbuild: {
-    // loader: "jsx",
-    // include: /src\/.*\.jsx?$/,
-    loader: "tsx", // 把所有的代码都当作tsx去处理，vite天生支持ts，而且jsx范围小于tsx，所以用tsx可以，jsx不行
-    include: /src\/.*\.[tj]sx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: "load-js-files-as-jsx",
-          setup(build) {
-            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({ // i modified the regex here
-              loader: "jsx",
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-        {
-          name: "load-tx-files-as-tsx",
-          setup(build) {
-            build.onLoad({ filter: /src\\.*\.ts$/ }, async (args) => ({ // i modified the regex here
-              loader: "tsx",
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-      ],
-    },
-  },
-  plugins: [
-    react(),
-    reactRefresh(),
-    svgr(),
-  ],
-  define: {
-    'process.env': process.env
-  },
-
-  // esbuild: {
-  //   loader: "jsx",
-  //   include: /src\/.*\.jsx?$/,
-  //   // loader: "tsx",
-  //   // include: /src\/.*\.[tj]sx?$/,
-  //   exclude: [],
-  // },
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     plugins: [
-  //       {
-  //         name: "load-js-files-as-jsx",
-  //         setup(build) {
-  //           build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
-  //             loader: "jsx",
-  //             contents: await fs.readFile(args.path, "utf8"),
-  //           }));
-  //         },
-  //       },
-  //     ],
-  //   },
-  // },
-
-  // esbuild: {
-  //   loader: 'jsx',
-  // },
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     loader: {
-  //       '.js': 'jsx',
-  //     },
-  //   },
-  // },
-
-  // css: {
-  //   preprocessorOptions: {
-  //     less: {
-  //       modifyVars: {
-  //         hack: `true; @import (reference) "${resolve("src/assets/less/index.less")}";`,
-  //       },
-  //       javascriptEnabled: true,
-  //     },
-  //   },
-  // },
-  server: {
-    port: 8002, // 设置服务启动端口号
-    open: true, // 默认打开浏览器
-    cors: true, // 允许跨域
-    proxy: {
-      '/api/': {
-        target: 'http://10.0.48.11:803/',
-        changeOrigin: true,
-        pathRewrite: { '^': '' },
+export default defineConfig(() => {
+  return {
+    assetsInclude: [
+      '**/*.awebp', '**/*.webp', '**/*.jpg', '**/*.jpeg',
+      '**/*.png', '**/*.docx', '**/*.doc', '**/*.pdf', '**/*.xlsx', '**/*.txt',
+      '**/*.pptx',
+    ],
+    resolve: {
+      alias: {
+        'src': resolve(__dirname,'src'),
       }
-    }
+    },
+    esbuild: {
+      // loader: "jsx",
+      // include: /src\/.*\.jsx?$/,
+      loader: "tsx", // 把所有的代码都当作tsx去处理，vite天生支持ts，而且jsx范围小于tsx，所以用tsx可以，jsx不行
+      include: /src\/.*\.[tj]sx?$/,
+      exclude: [],
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [
+          {
+            name: "load-js-files-as-jsx",
+            setup(build) {
+              build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({ // i modified the regex here
+                loader: "jsx",
+                contents: await fs.readFile(args.path, "utf8"),
+              }));
+            },
+          },
+          {
+            name: "load-tx-files-as-tsx",
+            setup(build) {
+              build.onLoad({ filter: /src\\.*\.ts$/ }, async (args) => ({ // i modified the regex here
+                loader: "tsx",
+                contents: await fs.readFile(args.path, "utf8"),
+              }));
+            },
+          },
+        ],
+      },
+    },
+    plugins: [
+      react(),
+      reactRefresh(),
+      svgr(),
+    ],
+    define: {
+      'process.env': process.env
+    },
 
+    // esbuild: {
+    //   loader: "jsx",
+    //   include: /src\/.*\.jsx?$/,
+    //   // loader: "tsx",
+    //   // include: /src\/.*\.[tj]sx?$/,
+    //   exclude: [],
+    // },
+    // optimizeDeps: {
+    //   esbuildOptions: {
+    //     plugins: [
+    //       {
+    //         name: "load-js-files-as-jsx",
+    //         setup(build) {
+    //           build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+    //             loader: "jsx",
+    //             contents: await fs.readFile(args.path, "utf8"),
+    //           }));
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
+
+    // esbuild: {
+    //   loader: 'jsx',
+    // },
+    // optimizeDeps: {
+    //   esbuildOptions: {
+    //     loader: {
+    //       '.js': 'jsx',
+    //     },
+    //   },
+    // },
+
+    // css: {
+    //   preprocessorOptions: {
+    //     less: {
+    //       modifyVars: {
+    //         hack: `true; @import (reference) "${resolve("src/assets/less/index.less")}";`,
+    //       },
+    //       javascriptEnabled: true,
+    //     },
+    //   },
+    // },
+    server: {
+      port: 8002, // 设置服务启动端口号
+      open: true, // 默认打开浏览器
+      cors: true, // 允许跨域
+      proxy: {
+        '/api/': {
+          target: 'http://10.0.48.11:803/',
+          changeOrigin: true,
+          pathRewrite: { '^': '' },
+        }
+      }
+
+    }
   }
 })
